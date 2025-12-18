00909c36b96a6cdc4ba50aded27de5f81b48f9f4
Jesjimrod <jesjimrod@alu.edu.gva.es>
2025-12-18 12:57:41 +0100

    Añadido método SaludoConEdad

--- src/Saludo.java
+++ src/Saludo.java
@@ -9,4 +9,20 @@ public class Saludo {
         System.out.println("Hola " +nombre);
         sc.close();
     }
+    public void SaludarConEdad() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime tu nombre");
+        String nombre = sc.nextLine();
+        System.out.println("Dime tu edad");
+        int edad = sc.nextInt();
+        sc.nextLine();
+
+        if (edad < 18) {
+            System.out.println("Hola " + nombre + ", eres menor de edad");
+        } else  {
+            System.out.println("Hola " + nombre + ", es mayor de edad");
+        }
+        sc.close();
+    }
 }
