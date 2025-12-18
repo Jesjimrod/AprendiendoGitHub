e4de136d68c457d4cf10a756f07f6b9c3ebf5d12
Jesjimrod <jesjimrod@alu.edu.gva.es>
2025-12-18 12:38:17 +0100

    Clase Saludo.java, Despedida.java y Principal.java, modificados

--- src/Despedida.java
+++ src/Despedida.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Despedida {
+    public void despedir(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Dime tu nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Hasta luego, " +  nombre);
+    }
 }
